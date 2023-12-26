84ffca13c5430595c7470f20254cc6835a2bf983
lrchere <128253749+lrchere@users.noreply.github.com>
2023-12-25 22:39:27 -0300

    Add single flutter engine (pre-warm) to render a single FlutterViewController

--- ios_app/ios_app/iosApp.swift
+++ ios_app/ios_app/iosApp.swift
@@ -1,14 +1,29 @@
 //
-//  ios_appApp.swift
-//  ios_app
+//  iosApp.swift
+//  iosApp
 //
 //  Created by A2 on 25/12/23.
 //
 
 import SwiftUI
+import Flutter
+import FlutterPluginRegistrant
+
+class AppDelegate: FlutterAppDelegate, ObservableObject {
+    let flutterEngine = FlutterEngine(name: "flutter_engine")
+
+    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        flutterEngine.run();
+        GeneratedPluginRegistrant.register(with: self.flutterEngine);
+
+        return true;
+    }
+}
 
 @main
 struct iosApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
