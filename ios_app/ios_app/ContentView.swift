63a2264fe54cea0144cb5d5e6a28b0177179ce87
lrchere <128253749+lrchere@users.noreply.github.com>
2023-12-25 22:46:59 -0300

    Add button and function to show a flutter screen from our iOS/Swift code

--- ios_app/ios_app/ContentView.swift
+++ ios_app/ios_app/ContentView.swift
@@ -6,17 +6,39 @@
 //
 
 import SwiftUI
+import Flutter
 
 struct ContentView: View {
+    @EnvironmentObject var appDelegate: AppDelegate
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello from iOS/Swift Screen!")
+            Button("Show Flutter Screen") {
+                showFlutterScreen()
+            }
         }
         .padding()
     }
+    
+    func showFlutterScreen() {
+        guard
+          let windowScene = UIApplication.shared.connectedScenes
+            .first(where: { $0.activationState == .foregroundActive && $0 is UIWindowScene }) as? UIWindowScene,
+          let window = windowScene.windows.first(where: \.isKeyWindow),
+          let rootViewController = window.rootViewController
+        else { return }
+
+
+        let flutterViewController = FlutterViewController(
+            engine: appDelegate.flutterEngine,
+          nibName: nil,
+          bundle: nil)
+        flutterViewController.modalPresentationStyle = .overCurrentContext
+        flutterViewController.isViewOpaque = false
+
+        rootViewController.present(flutterViewController, animated: true)
+      }
 }
 
 #Preview {
