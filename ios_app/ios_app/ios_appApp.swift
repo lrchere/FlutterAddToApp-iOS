460220947b6d012db6102822ad2ba3e5053b38f8
lrchere <128253749+lrchere@users.noreply.github.com>
2023-12-25 21:30:40 -0300

    Add iOS project

---
+++ ios_app/ios_app/ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appApp.swift
+//  ios_app
+//
+//  Created by A2 on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
